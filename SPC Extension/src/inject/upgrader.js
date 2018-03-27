3979924f66fa14815c5fead0cf2d7a226110fe20
Pablo Esco <pabloesco@bugz4amphetz.private>
2018-03-26 18:58:36 -0700

    Upgrade tester ++

--- SPC Extension/src/inject/upgrader.js
+++ SPC Extension/src/inject/upgrader.js
@@ -93,6 +93,7 @@ function run_tests() {
 		print_report(gWins, gLosses, getBetPercentage());
     	setTimeout(function () {
     		if (gRunCount < gRunTarget) { run_tests(); } else { 
+    			SPC_showresults();
 				print_report(gWins, gLosses, getBetPercentage());
 	            gRunCount = 0; 
 	            gSPCIsRunning = false;
@@ -116,12 +117,142 @@ function doRuns(runTargetNumber) {
 	run_tests();
 }
 
+function SPC_showresults() {
+	// remove the SPC testing row
+	document.getElementsByClassName("row spcUpgradeMain")[0].remove();
+	// re-setup essentially the same as makerow
+	var SPCRow = document.createElement("div");
+	SPCRow.className = "row spcUpgradeMainStats";
+	SPCRow.style.height = "280px";
+	SPCRow.style.marginBottom = "50px";
+	// SPC Title
+	var title =  document.createElement("p");
+	title.className = "spcTitle";
+	title.innerHTML = "&#9752; SPC &#9752;";
+	// SPC Run Tests Title
+	var subtitle = document.createElement("p");
+	subtitle.className = "spcSmallTitle";
+	subtitle.innerHTML = "Upgrade Theoretical Stats";
+	// styled seperator
+    var spcSeperator = document.createElement("hr");
+    spcSeperator.style.border = "0";
+    spcSeperator.style.height = "1px";
+    spcSeperator.style.backgroundImage = "linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0))";
+    // upgrade stats div
+	var caseStats = document.createElement("div");
+	caseStats.className = "case-winning-stats";
+	caseStats.style.backgroundColor = "#6f7290";
+
+	// wins container
+	var statWins = document.createElement("div");
+	statWins.className = "stat";
+	statWins.style.width = "auto";
+	// title
+	var statBigLabelWins = document.createElement("h5");
+	statBigLabelWins.innerHTML = "Wins";
+	// value
+	var statSmallLabelWins = document.createElement("h3");
+	statSmallLabelWins.innerHTML = gWins;
+	// glue together stat
+	statWins.appendChild(statBigLabelWins);
+	statWins.appendChild(statSmallLabelWins);
+	caseStats.appendChild(statWins);
+
+	// losses container
+	var statLosses = document.createElement("div");
+	statLosses.className = "stat";
+	statLosses.style.width = "auto";
+	// title
+	var statBigLabelLosses = document.createElement("h5");
+	statBigLabelLosses.innerHTML = "Losses";
+	// value
+	var statSmallLabelLosses = document.createElement("h3");
+	statSmallLabelLosses.innerHTML = gLosses;
+	// glue together stat
+	statLosses.appendChild(statBigLabelLosses);
+	statLosses.appendChild(statSmallLabelLosses);
+	caseStats.appendChild(statLosses);
+
+	// winrate container
+	var statWinrate = document.createElement("div");
+	statWinrate.className = "stat";
+	statWinrate.style.width = "auto";
+	// title
+	var statBigLabelWinrate = document.createElement("h5");
+	statBigLabelWinrate.innerHTML = "Win rate";
+	// value
+	var statSmallLabelWinrate = document.createElement("h3");
+	statSmallLabelWinrate.innerHTML = ((gWins/(gWins+gLosses))*100).toFixed(0)+"%";
+	// glue together stat
+	statWinrate.appendChild(statBigLabelWinrate);
+	statWinrate.appendChild(statSmallLabelWinrate);
+	caseStats.appendChild(statWinrate);
+
+	// profit container
+	var statProfit = document.createElement("div");
+	statProfit.className = "stat";
+	statProfit.style.width = "auto";
+	// title
+	var statBigLabelProfit = document.createElement("h5");
+	statBigLabelProfit.innerHTML = "Profit";
+	// value
+	var statSmallLabelProfit = document.createElement("h3");
+	statSmallLabelProfit.innerHTML = "$"+gProfit.toFixed(0);
+	// glue together stat
+	statProfit.appendChild(statBigLabelProfit);
+	statProfit.appendChild(statSmallLabelProfit);
+	caseStats.appendChild(statProfit);
+
+	// spent container
+	var statSpent = document.createElement("div");
+	statSpent.className = "stat";
+	statSpent.style.width = "auto";
+	// title
+	var statBigLabelSpent = document.createElement("h5");
+	statBigLabelSpent.innerHTML = "Spent";
+	// value
+	var statSmallLabelSpent = document.createElement("h3");
+	statSmallLabelSpent.innerHTML = "$"+gSpent.toFixed(0);
+	// glue together stat
+	statSpent.appendChild(statBigLabelSpent);
+	statSpent.appendChild(statSmallLabelSpent);
+	caseStats.appendChild(statSpent);
+
+	// reset SPC
+	var resetBtn = document.createElement("div");
+	resetBtn.className = "spcResetButton";
+	resetBtn.innerHTML = "Reset";
+	resetBtn.style.backgroundColor = "#E94C4F";
+	resetBtn.onclick = function () {
+		gRunTarget = 0;
+		gRunCount = 0;
+		gWins = 0;
+		gLosses = 0;
+		gSpent = 0;
+		gProfit = 0;
+		gBigDataContainer = [];
+		gBigData = [];
+		document.getElementsByClassName("row spcUpgradeMainStats")[0].remove();
+		SPC_makerow();
+	};
+
+	// glue the new row in
+	SPCRow.appendChild(title);
+	SPCRow.appendChild(spcSeperator);
+	SPCRow.appendChild(subtitle);
+	SPCRow.appendChild(caseStats);
+	SPCRow.appendChild(document.createElement("br"));
+	SPCRow.appendChild(resetBtn);
+	document.getElementsByClassName("upgrade-body")[0].getElementsByClassName("row actions")[0].appendChild(SPCRow);
+}
+
 // Our initial SPC testing row
 function SPC_makerow() {
 	// setup the SPC div 
 	var SPCRow = document.createElement("div");
 	SPCRow.className = "row spcUpgradeMain";
 	SPCRow.style.height = "200px";
+	SPCRow.style.marginBottom = "50px";
 	// SPC Title
 	var title =  document.createElement("p");
 	title.className = "spcTitle";
